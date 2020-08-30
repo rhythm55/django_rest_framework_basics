13d8862747cce07db791dd5604ecefc728640b7e
Rhythm Thakur <rhythmthakur@Rhythms-MacBook-Air.local>
2020-08-30 23:22:19 +0530

    added admin user

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -12,28 +12,28 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('User must have a email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email,name=name)
+        user = self.model(email=email,name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-        def create_superuser(self,email,name,password):
-            """Create and save a new superuser with given details"""
-            user = self.create_user(email, name, password)
+    def create_superuser(self,email,name,password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            user.is_superuser = True
-            user.is_staff = True
-            user.save(using=self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-            return user
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255),
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 

2ea6d52d1eb4a1bb51e8ad80c0993329eeeca1bf
Rhythm Thakur <rhythmthakur@Rhythms-MacBook-Air.local>
2020-09-17 00:48:43 +0530

    deployement changes

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/rhythm55/django_rest_framework_basics.git'
+PROJECT_GIT_URL='https://github.com/rhythm55/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
